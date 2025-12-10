17acd85273970bdab1a3900a7ea5e1104136dd74
Atilla Kati (Company) <atilla.kati@variosystems.com>
2025-12-10 11:18:49 +0100

    Change Project url and add logo

--- 20251209/Wifi.Toolbox/Wifi.Toolbox.TestApplication/Program.cs
+++ 20251209/Wifi.Toolbox/Wifi.Toolbox.TestApplication/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Wifi.Toolbox.Tools;
 
 namespace Wifi.Toolbox.TestApplication
@@ -11,7 +7,7 @@ namespace Wifi.Toolbox.TestApplication
     {
         static void Main(string[] args)
         {
-            ConsoleTools.CreateHeader("Demo Application ConsoleTools");
+            ConsoleTools.CreateHeader("Demo Application ConsoleTools");            
 
             int geburtsJahr = ConsoleTools.GetInt("Bitte Geburtsjahr eingeben: ",
                 DateTime.Now.Year - 150, DateTime.Now.Year - 5);
