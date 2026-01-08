399dfd66e15709f3e63adb1594fc9e7551687449
Atilla Kati (WIFI) <atilla@kati.bz>
2026-01-08 20:06:09 +0100

    New datatype creation: enum basics

---
+++ 20260108/GL_enum/GL_enum/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace GL_enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PowerState state = PowerState.Off;
+
+            state = PowerState.Standby;
+
+            Console.WriteLine("State: " + state);
+
+            if (state > PowerState.Off)
+            {
+                Console.WriteLine("Rechner läuft!");
+            }
+        }
+    }
+}
