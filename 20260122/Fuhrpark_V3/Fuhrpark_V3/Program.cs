d7641a8864d87875c2f4d5242ccbdee7a62d1513
Atilla Kati (WIFI) <atilla@kati.bz>
2026-01-22 19:51:08 +0100

    Polymorphie Grundlagen

---
+++ 20260122/Fuhrpark_V3/Fuhrpark_V3/Program.cs
@@ -0,0 +1,28 @@
+﻿using Fuhrpark_V3.Types;
+
+namespace Fuhrpark_V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myVehicleList = new Vehicle[]
+            {
+                new Car("Fiat e500 City", 165, ConsoleColor.Magenta),
+                new Scooter(),
+                new Scooter("Mikey Mouse Scooter", 35, 150),
+                new RacingCar("BadMobil F1 Edition", 385, ConsoleColor.Gray, DownforceType.Wings),
+            };
+
+            ShowVehicles(myVehicleList);
+        }
+
+        private static void ShowVehicles(Vehicle[] myVehicleList)
+        {
+            foreach (var vehicle in myVehicleList)
+            {
+                vehicle.Show();                           
+            }
+        }
+    }
+}
---
+++ 20260122/Fuhrpark_V3/Fuhrpark_V3/Types/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuhrpark_V3.Types
+{
+    internal class Car : Vehicle
+    {
+        private int _seatCount;        
+
+        public Car(string description, int maxSpeed, ConsoleColor color)
+            : base(description, maxSpeed, color)
+        {
+            _seatCount = 5;        
+        }
+
+        public Car(string description, int maxSpeed, ConsoleColor color, int seatCount)
+            : base(description, maxSpeed, color)
+        {
+            _seatCount = seatCount;
+        }
+
+        public int SeatCount
+        {
+            get { return _seatCount; }
+        }
+
+        public override void Show()
+        {
+            base.Show();
+            Console.WriteLine($"  => Sitzplätze: {_seatCount}");
+        }
+    }
+}
