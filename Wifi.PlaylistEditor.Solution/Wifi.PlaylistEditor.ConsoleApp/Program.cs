b44cd830efa77f244982ef3a5e2f8bf0feeba56f
Atilla Kati (WIFI) <atilla@kati.bz>
2026-02-10 21:27:14 +0100

    Add simple Consolen application to test the backend

--- Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.ConsoleApp/Program.cs
+++ Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Wifi.PlaylistEditor.BaseTypes;
+using Wifi.PlaylistEditor.Factories;
+using Wifi.PlaylistEditor.RepositoryTypes.M3u;
 
 namespace Wifi.PlaylistEditor.ConsoleApp
 {
@@ -6,9 +8,44 @@ namespace Wifi.PlaylistEditor.ConsoleApp
     {
         static void Main(string[] args)
         {
+            var playlistItemFactory = new PlaylistItemFactory();
+
+            //var filePathList = new string[]
+            //{
+            //    @"C:\Users\User\Music\DemoFiles\001 - Bruno Mars - Grenade.mp3",
+            //    @"C:\Users\User\Music\DemoFiles\01 - Broken Pieces (feat. Nihils).mp3",
+            //    @"C:\Users\User\Music\DemoFiles\002 - Lena - Taken By A Stranger.mp3",
+            //    @"C:\Users\User\Music\DemoFiles\003 - Adele - Rolling in the deep.mp3",
+            //    @"C:\Users\User\Music\DemoFiles\004 - Hurts - Stay.mp3",
+            //    @"C:\Users\User\Music\DemoFiles\005 - Taio Cruz feat. Kylie Minogue - Higher.mp3",
+            //    @"C:\Users\User\Music\DemoFiles\006 - Lady Gaga - Born This Way.mp3",
+            //    @"C:\Users\User\Music\DemoFiles\brettspiel.jpg",
+            //    @"C:\Users\User\Music\DemoFiles\gettyimages-929531216-594x594.jpg",
+            //    @"C:\Users\User\Music\DemoFiles\istockphoto-1150931120-612x612.jpg",
+            //    @"C:\Users\User\Music\DemoFiles\Kayla-Person.jpg",
+            //    @"C:\Users\User\Music\DemoFiles\pexels-mikebirdy-170811.jpg",
+            //    @"C:\Users\User\Music\DemoFiles\Sizilianischer_Auto.jpg"
+            //};
+
+            if (args.Length == 0)
+            {
+                return;
+            }
+
             var playlist = new Playlist("Demo Playlist Charts 2026", "DJ Gandalf");
 
+            foreach (var filePath in args)
+            {
+                var item = playlistItemFactory.Create(filePath);
+                if(item != null)
+                {
+                    playlist.Add(item);
+                }
+            }
 
+            //Playlist Datei generieren
+            var repository = new M3uRepository(playlistItemFactory);
+            repository.Save(playlist, playlist.Title + ".m3u");
         }
     }
 }
